b0d4f8a47c578aebba822316b1b8bb2e0f1e60d2
Emanuel Martinez <emanuel.mg86@gmail.com>
2024-03-12 06:13:06 -0300

    Se crea repo

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'young': ['Young Serif', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
